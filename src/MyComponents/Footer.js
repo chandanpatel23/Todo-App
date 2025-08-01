5eb2d266ac3af345d2dd19513c5865d4bec82639
Chandan Patel <chandanpatel23@gmail.com>
2025-08-01 18:49:57 +0530

    Final Commit

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
-const Footer = () => {
-  
-  return (
-    <footer className='bg-dark text-light my-2 py-2 text-center' >
-      <p>Copyright &copy; MyTodosList.com
-      All rights reserved
-      Last updated: {new Date().toLocaleDateString()}</p>
-      
-    </footer>
-  )
-}
 
-export default Footer
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">
+            Copyright &copy; MyTodosList.com
+            </p>
+        </footer>
+    )
+}
