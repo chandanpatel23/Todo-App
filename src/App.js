3d74d100f11fb284df2f8ebb369cc104029a0568
Chandan Patel <chandanpatel23@gmail.com>
2025-08-01 18:23:53 +0530

    Adding todo added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,81 @@
-
 import './App.css';
 import Header from "./MyComponents/Header";
-import Todos from "./MyComponents/Todos";
-import Footer from "./MyComponents/Footer";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { About } from "./MyComponents/About";
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-    const onDelete = (todo) => {
-        console.log("I am onDelete of todo");
-    }
-  let todos =[
-    {
-      sno: 1,
-      title: "Go to the market",
-      desc: "You need to go to the market to buy vegetables"
-    },
-    {
-      sno: 2,
-      title: "Go to the mall",
-      desc: "You need to go to the mall to buy clothes"
-    },
-    {
-      sno: 3,
-      title: "Go to the gym",
-      desc: "You need to go to the gym to stay fit"
-    }
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
 
-    ]
-  
-  return (
-    
-    
-    <>
-   <Header title = "My Todos List" searchBar = {false} />
-   <Todos todos = {todos} onDelete={onDelete}/>
-   <Footer/>
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    // Deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return ( 
+    <> 
+    <Router>
+      <Header title="My Todos List" searchBar={false} /> 
+      <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+            <>
+            <AddTodo addTodo={addTodo} />
+            <Todos todos={todos} onDelete={onDelete} /> 
+            </>)
+          }}> 
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route> 
+        </Switch> 
+      <Footer />
+    </Router>
     </>
   );
 }
--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
+  
   return (
-    <div>
-      footer works!
-    </div>
+    <footer className='bg-dark text-light my-2 py-2 text-center' >
+      <p>Copyright &copy; MyTodosList.com
+      All rights reserved
+      Last updated: {new Date().toLocaleDateString()}</p>
+      
+    </footer>
   )
 }
 
